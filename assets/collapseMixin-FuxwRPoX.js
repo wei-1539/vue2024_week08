55db810c8550ac96d095cf0b9f0a9a7abe85a26d
Rio Wei <wutony0713@gmail.com>
2024-04-14 11:31:12 +0800

    Updates

--- assets/collapseMixin-FuxwRPoX.js
+++ assets/collapseMixin-FuxwRPoX.js
@@ -1,4 +1,4 @@
-import{G as P,H as F}from"./index-cDLFXX9z.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-BwXYVkrl.js";var A={exports:{}};/*!
+import{G as P,H as F}from"./index-8zm4eLX_.js";import{r as H,a as V,b as q,c as z}from"./selector-engine-jlJJXIKX.js";var A={exports:{}};/*!
   * Bootstrap collapse.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
